48d089c2749c779c08269e68d1c2c930e4a3ff9e
AF-Nexus <afnexus01@gmail.com>
2025-04-27 20:12:01 +0200

    Update raiden.js

--- raiden.js
+++ raiden.js
@@ -1,26 +1,50 @@
-const express = require('express');
-const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import bodyParser from 'body-parser';
+import { EventEmitter } from 'events';
+
+const __filename = fileURLToPath(import.meta.url);
+const __path = dirname(__filename);
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
+
+// Import your modules using dynamic imports or convert them to ES modules too
+import('./qr.js').then(module => {
+  const server = module.default || module;
+  
+  import('./pair.js').then(module => {
+    const code = module.default || module;
+    
+    const app = express();
+    
+    // Set max listeners
+    EventEmitter.defaultMaxListeners = 500;
+    
+    // Set up routes
+    app.use('/qr', server);
+    app.use('/code', code);
+    
+    app.use('/pair', async (req, res) => {
+      res.sendFile(`${__path}/pair.html`);
+    });
+    
+    app.use('/', async (req, res) => {
+      res.sendFile(`${__path}/main.html`);
+    });
+    
+    // Body parser middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`
 Powered by The Developer03
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+ Server running on http://localhost:${PORT}`);
+    });
+    
+    // Export app (as default export in ES modules)
+    export default app;
+  });
+});
